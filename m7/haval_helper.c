2b41700089b1b820ed214d30ed8643b6dce1ae5f
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2025-05-01 15:24:14 -0500

    Update haval_helper.c

--- m7/haval_helper.c
+++ m7/haval_helper.c
@@ -1,194 +1,126 @@
-/* $Id: haval_helper.c 218 2010-06-08 17:06:34Z tp $ */
-/*
- * Helper code, included (three times !) by HAVAL implementation.
- *
- * TODO: try to merge this with md_helper.c.
- *
- * ==========================(LICENSE BEGIN)============================
- *
- * Copyright (c) 2007-2010  Projet RNRT SAPHIR
- * 
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- * 
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * ===========================(LICENSE END)=============================
- *
- * @author   Thomas Pornin <thomas.pornin@cryptolog.com>
- */
+// haval_helper.c - Core passes and finishing functions for HAVAL
 
-#undef SPH_XCAT
-#define SPH_XCAT(a, b)    SPH_XCAT_(a, b)
-#undef SPH_XCAT_
-#define SPH_XCAT_(a, b)   a ## b
+#include <stddef.h>
+#include <string.h>
+#include "sph_haval.h"
 
-static void
-#ifdef SPH_UPTR
-SPH_XCAT(SPH_XCAT(haval, PASSES), _short)
+#if !defined(PASSES)
+#error "PASSES macro must be defined before including haval_helper.c"
+#endif
+
+// Context type
+typedef sph_haval_context context_t;
+
+// Internal block size for HAVAL: 1024 bits = 128 bytes
+#define HAVAL_BLOCK_SIZE 128
+
+// Forward declarations of generic core and finish functions
+static void haval_update(context_t *sc, const void *data, size_t len);
+static void haval_finish(context_t *sc, unsigned ub, unsigned n, void *dst);
+
+// Transformation function wrapper selected by PASSES
+static void haval_core(context_t *sc, const unsigned char *data) {
+	DSTATE;
+	RSTATE;
+
+#if PASSES == 3
+	// Prepare message words
+#if SPH_LITTLE_FAST
+	const unsigned char *load_ptr = data;
+#define INMSG(i) sph_dec32le_aligned(load_ptr + 4*(i))
+#elif !SPH_LITTLE_FAST
+	sph_u32 msg[32];
+	for (int i=0; i<32; i++)
+		msg[i] = sph_dec32le_aligned(data + 4*i);
+#define INMSG(i) msg[i]
 #else
-SPH_XCAT(haval, PASSES)
+#error "Endianness setting incompatible in haval_helper"
 #endif
-(sph_haval_context *sc, const void *data, size_t len)
-{
-	unsigned current;
 
-#if SPH_64
-	current = (unsigned)sc->count & 127U;
+	CORE3(INMSG);
+#elif PASSES == 4
+#if SPH_LITTLE_FAST
+	const unsigned char *load_ptr = data;
+#define INMSG(i) sph_dec32le_aligned(load_ptr + 4*(i))
+#elif !SPH_LITTLE_FAST
+	sph_u32 msg[32];
+	for (int i=0; i<32; i++)
+		msg[i] = sph_dec32le_aligned(data + 4*i);
+#define INMSG(i) msg[i]
 #else
-	current = (unsigned)sc->count_low & 127U;
-#endif
-	while (len > 0) {
-		unsigned clen;
-#if !SPH_64
-		sph_u32 clow, clow2;
+#error "Endianness setting incompatible in haval_helper"
 #endif
 
-		clen = 128U - current;
-		if (clen > len)
-			clen = len;
-		memcpy(sc->buf + current, data, clen);
-		data = (const unsigned char *)data + clen;
-		current += clen;
-		len -= clen;
-		if (current == 128U) {
-			DSTATE;
-			IN_PREPARE(sc->buf);
-
-			RSTATE;
-			SPH_XCAT(CORE, PASSES)(INW);
-			WSTATE;
-			current = 0;
-		}
-#if SPH_64
-		sc->count += clen;
+	CORE4(INMSG);
+#elif PASSES == 5
+#if SPH_LITTLE_FAST
+	const unsigned char *load_ptr = data;
+#define INMSG(i) sph_dec32le_aligned(load_ptr + 4*(i))
+#elif !SPH_LITTLE_FAST
+	sph_u32 msg[32];
+	for (int i=0; i<32; i++)
+		msg[i] = sph_dec32le_aligned(data + 4*i);
+#define INMSG(i) msg[i]
 #else
-		clow = sc->count_low;
-		clow2 = SPH_T32(clow + clen);
-		sc->count_low = clow2;
-		if (clow2 < clow)
-			sc->count_high ++;
+#error "Endianness setting incompatible in haval_helper"
 #endif
-	}
+
+	CORE5(INMSG);
+#endif
 }
 
-#ifdef SPH_UPTR
-static void
-SPH_XCAT(haval, PASSES)(sph_haval_context *sc, const void *data, size_t len)
-{
-	unsigned current;
-	size_t orig_len;
-#if !SPH_64
-	sph_u32 clow, clow2;
-#endif
-	DSTATE;
+// Public API functions invoked by main HAVAL implementation for update/finalize
 
-	if (len < 256U) {
-		SPH_XCAT(SPH_XCAT(haval, PASSES), _short)(sc, data, len);
-		return;
-	}
-#if SPH_64
-	current = (unsigned)sc->count & 127U;
-#else
-	current = (unsigned)sc->count_low & 127U;
-#endif
-	if (current > 0) {
-		unsigned clen;
+void haval##PASSES(context_t *sc, const void *data, size_t len) {
+	const unsigned char *buf = (const unsigned char *)data;
+	size_t used = (sc->count & 0x7F);
+	size_t fill = HAVAL_BLOCK_SIZE - used;
 
-		clen = 128U - current;
-		SPH_XCAT(SPH_XCAT(haval, PASSES), _short)(sc, data, clen);
-		data = (const unsigned char *)data + clen;
-		len -= clen;
-	}
-#if !SPH_UNALIGNED
-	if (((SPH_UPTR)data & 3U) != 0) {
-		SPH_XCAT(SPH_XCAT(haval, PASSES), _short)(sc, data, len);
-		return;
-	}
-#endif
-	orig_len = len;
-	RSTATE;
-	while (len >= 128U) {
-		IN_PREPARE(data);
+	sc->count += (unsigned)len;
 
-		SPH_XCAT(CORE, PASSES)(INW);
-		data = (const unsigned char *)data + 128U;
-		len -= 128U;
+	if (used && len >= fill) {
+		memcpy(&sc->buf[used], buf, fill);
+		haval_core(sc, sc->buf);
+		buf += fill;
+		len -= fill;
+		used = 0;
 	}
-	WSTATE;
-	if (len > 0)
-		memcpy(sc->buf, data, len);
-#if SPH_64
-	sc->count += (sph_u64)orig_len;
-#else
-	clow = sc->count_low;
-	clow2 = SPH_T32(clow + orig_len);
-	sc->count_low = clow2;
-	if (clow2 < clow)
-		sc->count_high ++;
-	orig_len >>= 12;
-	orig_len >>= 10;
-	orig_len >>= 10;
-	sc->count_high += orig_len;
-#endif
+
+	while (len >= HAVAL_BLOCK_SIZE) {
+		haval_core(sc, buf);
+		buf += HAVAL_BLOCK_SIZE;
+		len -= HAVAL_BLOCK_SIZE;
+	}
+
+	if (len > 0) {
+		memcpy(&sc->buf[used], buf, len);
+	}
+
+	sc->count_low = (unsigned)(sc->count & 0xFFFFFFFF);
+	sc->count_high = (unsigned)(sc->count >> 32);
 }
-#endif
 
-static void
-SPH_XCAT(SPH_XCAT(haval, PASSES), _close)(sph_haval_context *sc,
-	unsigned ub, unsigned n, void *dst)
-{
-	unsigned current;
-	DSTATE;
+void haval##PASSES##_close(context_t *sc, unsigned ub, unsigned n, void *dst) {
+	unsigned char *buf = sc->buf;
+	unsigned used = (unsigned)(sc->count & 0x7F);
 
-#if SPH_64
-	current = (unsigned)sc->count & 127U;
-#else
-	current = (unsigned)sc->count_low & 127U;
-#endif
-	sc->buf[current ++] = (0x01 << n) | ((ub & 0xFF) >> (8 - n));
-	RSTATE;
-	if (current > 118U) {
-		memset(sc->buf + current, 0, 128U - current);
+	// Add padding
+	buf[used ++] = 0x01;
 
-		do {
-			IN_PREPARE(sc->buf);
-
-			SPH_XCAT(CORE, PASSES)(INW);
-		} while (0);
-		current = 0;
+	if (used > HAVAL_BLOCK_SIZE - 10) {
+		memset(buf + used, 0, HAVAL_BLOCK_SIZE - used);
+		haval_core(sc, buf);
+		used = 0;
 	}
-	memset(sc->buf + current, 0, 118U - current);
-	sc->buf[118] = 0x01 | (PASSES << 3);
-	sc->buf[119] = sc->olen << 3;
-#if SPH_64
-	sph_enc64le_aligned(sc->buf + 120, SPH_T64(sc->count << 3));
-#else
-	sph_enc32le_aligned(sc->buf + 120, SPH_T32(sc->count_low << 3));
-	sph_enc32le_aligned(sc->buf + 124,
-		SPH_T32((sc->count_high << 3) | (sc->count_low >> 29)));
-#endif
-	do {
-		IN_PREPARE(sc->buf);
 
-		SPH_XCAT(CORE, PASSES)(INW);
-	} while (0);
+	memset(buf + used, 0, HAVAL_BLOCK_SIZE - used - 10);
 
-	WSTATE;
+	// Add bit counts and fingerprint selector
+	sph_enc64le(buf + HAVAL_BLOCK_SIZE - 10, sc->count << 3);
+
+	buf[HAVAL_BLOCK_SIZE - 2] = (unsigned char)((sc->olen * 8) & 0xFF);
+	buf[HAVAL_BLOCK_SIZE - 1] = (unsigned char)((PASSES << 3) | (sc->olen & 7));
+
+	haval_core(sc, buf);
 	haval_out(sc, dst);
-	haval_init(sc, sc->olen, sc->passes);
 }
